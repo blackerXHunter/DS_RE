a33c6372f29c319e6654549a657a22ba0f055027
blacXhunt <blackerXHunter@outlook.com>
2018-09-14 15:57:17 +0800

    damage animation

---
+++ Assets/Scripts/BattleManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CapsuleCollider))]
+public class BattleManager : MonoBehaviour {
+
+    [SerializeField]
+    private Animator animator;
+
+    private void OnTriggerEnter(Collider other) {
+        animator.SetTrigger("damage");
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
