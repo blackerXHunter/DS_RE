2f8e1af8f0c94207b15c3a859ac737147bf59508
blackerXHunter <blackerXHunter@outlook.com>
2018-10-06 21:17:28 +0800

    timeline & example

---
+++ Assets/DirectorTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class DirectorTest : MonoBehaviour {
+
+    public PlayableDirector pd;
+
+    public Animator attackerAnim;
+    public Animator victimerAnim;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey( KeyCode.H)) {
+            foreach (var track in pd.playableAsset.outputs) {
+                if (track.streamName == "Attacker Animation") {
+                    pd.SetGenericBinding(track.sourceObject, attackerAnim);
+                }
+                else if (track.streamName == "Victim Animation") {
+                    pd.SetGenericBinding(track.sourceObject, victimerAnim);
+                }
+            }
+            pd.Play();
+        }
+	}
+}
