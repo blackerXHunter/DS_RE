2f30bbcb0ece443e3346c7b2efb9f7189d5939cc
blackerXHunter <blackerXHunter@outlook.com>
2019-03-29 00:29:07 +0800

    fungus init

---
+++ Assets/_Main/Scripts/Talk/TalkManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TalkManager : MonoBehaviour
+{
+
+    public Fungus.Flowchart chat;
+    // Start is called before the first frame update
+    void Start()
+    {
+        chat = GetComponent<Fungus.Flowchart>();
+        chat.SendFungusMessage("Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
