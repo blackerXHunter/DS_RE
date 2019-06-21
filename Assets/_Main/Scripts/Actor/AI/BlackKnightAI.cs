b473b0f7aaf799e55c684e83f90422ae8e0b077f
blackerXHunter <blackerXHunter@outlook.com>
2019-06-22 02:19:43 +0800

    aa

--- Assets/_Main/Scripts/Actor/AI/BlackKnightAI.cs
+++ Assets/_Main/Scripts/Actor/AI/BlackKnightAI.cs
@@ -7,8 +7,7 @@ using LiquidState;
 using LiquidState.Synchronous.Core;
 using UnityEngine;
 
-public class BlackKnightAI : MonoBehaviour
-{
+public class BlackKnightAI : MonoBehaviour {
 
     private ActorManager am;
     public float speed = 1.0f;
@@ -17,55 +16,43 @@ public class BlackKnightAI : MonoBehaviour
     [SerializeField]
     private float canAttackDistance = 1.4f;
 
-
     #region Mono
-    void Start()
-    {
-        if (am == null)
-        {
-            am = GetComponent<ActorManager>();
+    void Start () {
+        if (am == null) {
+            am = GetComponent<ActorManager> ();
         }
-        Configure();
-        if (patrolPoints != null && patrolPoints.Length > 0)
-        {
-            fsm.Fire(BlackKnightTrigger.Patrol);
+        Configure ();
+        if (patrolPoints != null && patrolPoints.Length > 0) {
+            fsm.Fire (BlackKnightTrigger.Patrol);
         }
     }
 
-    void Update()
-    {
-        if (playerTansform != null && fsm != null)
-        {
-            if (Vector3.Distance(this.transform.position, playerTansform.position) < canAttackDistance && fsm.CurrentState == BlackKnightState.Following)
-            {
-                fsm.Fire(BlackKnightTrigger.Touch);
-            }
-            else
-            if (Vector3.Distance(this.transform.position, playerTansform.position) >= canAttackDistance && fsm.CurrentState == BlackKnightState.Attcking)
-            {
-                fsm.Fire(BlackKnightTrigger.UnTouch);
+    void Update () {
+        if (playerTansform != null && fsm != null) {
+            if (Vector3.Distance (this.transform.position, playerTansform.position) < canAttackDistance && fsm.CurrentState == BlackKnightState.Following) {
+                fsm.Fire (BlackKnightTrigger.Touch);
+            } else
+            if (Vector3.Distance (this.transform.position, playerTansform.position) >= canAttackDistance && fsm.CurrentState == BlackKnightState.Attcking) {
+                fsm.Fire (BlackKnightTrigger.UnTouch);
             }
         }
     }
 
-    void OnDestroy()
-    {
-        partrolTaskCTS?.Cancel();
-        findCTS?.Cancel();
+    void OnDestroy () {
+        partrolTaskCTS?.Cancel ();
+        findCTS?.Cancel ();
     }
     #endregion
 
     #region State Machine
-    enum BlackKnightState
-    {
+    enum BlackKnightState {
         Idle,
         Patroling,
         Attcking,
         Following,
         Confrontation
     }
-    enum BlackKnightTrigger
-    {
+    enum BlackKnightTrigger {
         Patrol,
         Found,
         FollowFail,
@@ -73,74 +60,63 @@ public class BlackKnightAI : MonoBehaviour
         UnTouch,
         Confrontation
     }
-    [ContextMenu("Partrol")]
-    public void Patrol()
-    {
-        fsm.Fire(BlackKnightTrigger.Patrol);
+
+    [ContextMenu ("Partrol")]
+    public void Patrol () {
+        fsm.Fire (BlackKnightTrigger.Patrol);
     }
-    [ContextMenu("Stop Follow")]
-    public void FollowFail()
-    {
-        fsm.Fire(BlackKnightTrigger.FollowFail);
+
+    [ContextMenu ("Stop Follow")]
+    public void FollowFail () {
+        fsm.Fire (BlackKnightTrigger.FollowFail);
     }
     private IStateMachine<BlackKnightState, BlackKnightTrigger> fsm;
-    void Configure()
-    {
-        var config = StateMachineFactory.CreateConfiguration<BlackKnightState, BlackKnightTrigger>();
+    void Configure () {
+        var config = StateMachineFactory.CreateConfiguration<BlackKnightState, BlackKnightTrigger> ();
 
-        config.ForState(BlackKnightState.Idle).Permit(BlackKnightTrigger.Patrol, BlackKnightState.Patroling);
+        config.ForState (BlackKnightState.Idle).Permit (BlackKnightTrigger.Patrol, BlackKnightState.Patroling);
 
-        config.ForState(BlackKnightState.Patroling).OnEntry(() =>
-        {
-            StartFind();
-            StartPatrol();
-        })
-        .OnExit(() =>
-        {
-            StopFind();
-            StopPatrol();
-        })
-        .Permit(BlackKnightTrigger.Found, BlackKnightState.Following);
+        config.ForState (BlackKnightState.Patroling).OnEntry (() => {
+                StartFind ();
+                StartPatrol ();
+            })
+            .OnExit (() => {
+                StopFind ();
+                StopPatrol ();
+            })
+            .Permit (BlackKnightTrigger.Found, BlackKnightState.Following);
 
-        config.ForState(BlackKnightState.Following).OnEntry(() =>
-       {
-           StartFollow();
-       })
-       .OnExit(() =>
-       {
-           StopFollow();
-       })
-       .Permit(BlackKnightTrigger.FollowFail, BlackKnightState.Patroling)
-       .Permit(BlackKnightTrigger.Touch, BlackKnightState.Attcking)
-       .Permit(BlackKnightTrigger.Confrontation, BlackKnightState.Confrontation);
+        config.ForState (BlackKnightState.Following).OnEntry (() => {
+                StartFollow ();
+            })
+            .OnExit (() => {
+                StopFollow ();
+            })
+            .Permit (BlackKnightTrigger.FollowFail, BlackKnightState.Patroling)
+            .Permit (BlackKnightTrigger.Touch, BlackKnightState.Attcking)
+            .Permit (BlackKnightTrigger.Confrontation, BlackKnightState.Confrontation);
 
-        config.ForState(BlackKnightState.Attcking).OnEntry(() =>
-        {
-            StartAutoAttack();
-            //Attack();
-        }).OnExit(() =>
-        {
-            StopAutoAttack();
-            //UnAttack();
-        })
-        .Permit(BlackKnightTrigger.UnTouch, BlackKnightState.Patroling)
-        ;
-        fsm = StateMachineFactory.Create(BlackKnightState.Idle, config);
+        config.ForState (BlackKnightState.Attcking).OnEntry (() => {
+                StartAutoAttack ();
+                //Attack();
+            }).OnExit (() => {
+                StopAutoAttack ();
+                //UnAttack();
+            })
+            .Permit (BlackKnightTrigger.UnTouch, BlackKnightState.Patroling);
+        fsm = StateMachineFactory.Create (BlackKnightState.Idle, config);
 
-        config.ForState(BlackKnightState.Confrontation)
-        .OnEntry(() =>
-        {
-            var enemyAC = am.ac as EnemyAC;
-            enemyAC.camCtrl.LockUnLock();
-        })
-        .OnExit(() =>
-        {
-            var enemyAC = am.ac as EnemyAC;
-            if (enemyAC.camCtrl.lockState == true)
-            {
-                enemyAC.camCtrl.LockUnLock();
-            }
-        });
+        config.ForState (BlackKnightState.Confrontation)
+            .OnEntry (() => {
+                var enemyAC = am.ac as EnemyAC;
+                enemyAC.camCtrl.LockUnLock ();
+            })
+            .OnExit (() => {
+                var enemyAC = am.ac as EnemyAC;
+                if (enemyAC.camCtrl.lockState == true) {
+                    enemyAC.camCtrl.LockUnLock ();
+                }
+            });
     }
     #endregion
 
@@ -148,45 +124,36 @@ public class BlackKnightAI : MonoBehaviour
     public Transform[] patrolPoints;
 
     public float waitTime;
-    [ContextMenu("Start Patrol Task")]
-    public async void StartPatrol()
-    {
-        if (patrolPoints == null || patrolPoints.Length <= 0)
-        {
-            Debug.Log("patrol points is not set");
+    [ContextMenu ("Start Patrol Task")]
+    public async void StartPatrol () {
+        if (patrolPoints == null || patrolPoints.Length <= 0) {
+            Debug.Log ("patrol points is not set");
             return;
         }
-        partrolTaskCTS?.Cancel();
-        partrolTaskCTS = new CancellationTokenSource();
+        partrolTaskCTS?.Cancel ();
+        partrolTaskCTS = new CancellationTokenSource ();
         var ct = partrolTaskCTS.Token;
-        using (var partrolTask = PatrolTask(true, ct))
-        {
+        using (var partrolTask = PatrolTask (true, ct)) {
             await partrolTask;
         }
     }
     CancellationTokenSource partrolTaskCTS;
-    [ContextMenu("Stop Patrol Task")]
-    public void StopPatrol()
-    {
-        if (partrolTaskCTS != null)
-        {
-            partrolTaskCTS.Cancel();
+    [ContextMenu ("Stop Patrol Task")]
+    public void StopPatrol () {
+        if (partrolTaskCTS != null) {
+            partrolTaskCTS.Cancel ();
         }
     }
-    private async Task PatrolTask(bool loop, CancellationToken cancellationToken)
-    {
+    private async Task PatrolTask (bool loop, CancellationToken cancellationToken) {
 
-        do
-        {
-            for (int i = 0; i < patrolPoints.Length; i++)
-            {
-                if (cancellationToken.IsCancellationRequested)
-                {
+        do {
+            for (int i = 0; i < patrolPoints.Length; i++) {
+                if (cancellationToken.IsCancellationRequested) {
                     loop = false;
                     return;
                 }
-                await MoveToThePointAsync(patrolPoints[i], cancellationToken);
-                await Task.Delay(TimeSpan.FromSeconds(waitTime));
+                await MoveToThePointAsync (patrolPoints[i], cancellationToken);
+                await Task.Delay (TimeSpan.FromSeconds (waitTime));
             }
         } while (loop);
 
@@ -196,220 +163,183 @@ public class BlackKnightAI : MonoBehaviour
     #region Find Player
     public Transform playerTansform;
     public CancellationTokenSource findCTS;
-    [ContextMenu("Start Find")]
-    public async void StartFind()
-    {
-        findCTS?.Cancel();
-        findCTS = new CancellationTokenSource();
+    [ContextMenu ("Start Find")]
+    public async void StartFind () {
+        findCTS?.Cancel ();
+        findCTS = new CancellationTokenSource ();
         var ct = findCTS.Token;
-        using (var task = FindTaskAsync(ct))
-        {
+        using (var task = FindTaskAsync (ct)) {
             await task;
         }
     }
 
-    private async Task FindTaskAsync(CancellationToken ct)
-    {
+    private async Task FindTaskAsync (CancellationToken ct) {
         GameObject player = null;
-        while (player == null)
-        {
-            if (ct.IsCancellationRequested)
-            {
+        while (player == null) {
+            if (ct.IsCancellationRequested) {
                 return;
             }
-            player = CheckFoundPlayer();
-            await Task.Delay((int)(Time.deltaTime * 1000));
+            player = CheckFoundPlayer ();
+            await Task.Delay ((int) (Time.deltaTime * 1000));
         }
-        partrolTaskCTS?.Cancel();
+        partrolTaskCTS?.Cancel ();
         playerTansform = player.transform;
-        fsm.Fire(BlackKnightTrigger.Found);
-        fsm.Fire(BlackKnightTrigger.Confrontation);
+        fsm.Fire (BlackKnightTrigger.Found);
+        fsm.Fire (BlackKnightTrigger.Confrontation);
         //await MoveToThePointAsync(playerTansform, ct);
     }
-    [ContextMenu("Stop Find")]
-    public void StopFind()
-    {
-        findCTS.Cancel();
+
+    [ContextMenu ("Stop Find")]
+    public void StopFind () {
+        findCTS.Cancel ();
     }
     #endregion
 
     #region Follow Player
     public CancellationTokenSource followCTS;
-    public async void StartFollow()
-    {
-        followCTS?.Cancel();
-        followCTS = new CancellationTokenSource();
+    public async void StartFollow () {
+        followCTS?.Cancel ();
+        followCTS = new CancellationTokenSource ();
         var ct = followCTS.Token;
-        using (var task = FollowTaskAsync(ct))
-        {
+        using (var task = FollowTaskAsync (ct)) {
             await task;
         }
     }
 
-    private async Task FollowTaskAsync(CancellationToken ct)
-    {
-        await MoveToThePointAsync(playerTansform, ct);
+    private async Task FollowTaskAsync (CancellationToken ct) {
+        await MoveToThePointAsync (playerTansform, ct);
     }
-    [ContextMenu("Stop Find")]
-    public void StopFollow()
-    {
-        followCTS?.Cancel();
+
+    [ContextMenu ("Stop Find")]
+    public void StopFollow () {
+        followCTS?.Cancel ();
     }
     #endregion
 
     #region Attack Auto
     public CancellationTokenSource autoAttackCTS;
-    [ContextMenu("Start Attak")]
-    public async void StartAutoAttack()
-    {
-        autoAttackCTS?.Cancel();
-        autoAttackCTS = new CancellationTokenSource();
-        await AutoAttackTask(autoAttackCTS.Token);
+    [ContextMenu ("Start Attak")]
+    public async void StartAutoAttack () {
+        autoAttackCTS?.Cancel ();
+        autoAttackCTS = new CancellationTokenSource ();
+        await AutoAttackTask (autoAttackCTS.Token);
     }
 
-    public async Task AutoAttackTask(CancellationToken ct)
-    {
-        while (true)
-        {
-            if (ct.IsCancellationRequested)
-            {
-                UnAttack();
+    public async Task AutoAttackTask (CancellationToken ct) {
+        while (true) {
+            if (ct.IsCancellationRequested) {
+                UnAttack ();
                 return;
             }
-            var canAttack = CheckCanAttackPlayer();
-            if (canAttack)
-            {
-                am.ac.model.transform.LookAt(playerTansform);
-                Attack();
+            var canAttack = CheckCanAttackPlayer ();
+            if (canAttack) {
+                am.ac.model.transform.LookAt (playerTansform);
+                Attack ();
+            } else {
+                UnAttack ();
             }
-            else
-            {
-                UnAttack();
-            }
-            await Task.Delay((int)(1000 * Time.deltaTime));
+            await Task.Delay ((int) (1000 * Time.deltaTime));
         }
     }
 
-    [ContextMenu("Stop Attack")]
-    public void StopAutoAttack()
-    {
-        UnAttack();
-        autoAttackCTS?.Cancel();
+    [ContextMenu ("Stop Attack")]
+    public void StopAutoAttack () {
+        UnAttack ();
+        autoAttackCTS?.Cancel ();
     }
     #endregion
 
     #region Confrontation
-        async void StartConfrontatoin(){
+    async void StartConfrontatoin () {
 
-        }
-        async Task ConfrontationTask(){
-            
-        }
-        async void StopConfrontatation(){
+    }
+    async Task ConfrontationTask () {
 
-        }
+    }
+    async void StopConfrontatation () {
+
+    }
     #endregion
 
     #region Utility
-    private async Task MoveToThePointAsync(Transform transform, CancellationToken cancellationToken)
-    {
+    private async Task MoveToThePointAsync (Transform transform, CancellationToken cancellationToken) {
 
-        while (!IsTouched(this.transform, transform))
-        {
+        while (!IsTouched (this.transform, transform)) {
 
-            if (cancellationToken.IsCancellationRequested)
-            {
+            if (cancellationToken.IsCancellationRequested) {
                 break;
             }
             Vector3 dir = transform.position - this.transform.position;
-            dir.Normalize();
-            this.transform.LookAt(dir);
-            Move(dir, speed, run);
-            await Task.Delay((int)(Time.deltaTime * 1000));
-            if (this == null)
-            {
+            dir.Normalize ();
+            this.transform.LookAt (dir);
+            Move (dir, speed, run);
+            await Task.Delay ((int) (Time.deltaTime * 1000));
+            if (this == null) {
                 return;
             }
         }
-        Debug.Log("OK");
-        Reset();
+        Debug.Log ("OK");
+        Reset ();
     }
 
-    private GameObject CheckFoundPlayer()
-    {
-        var cols = Physics.OverlapBox(this.transform.position, new Vector3(0.5f, 0.5f, 5), am.ac.model.transform.rotation, LayerMask.GetMask("Player"));
-        if (cols.Length > 0)
-        {
+    private GameObject CheckFoundPlayer () {
+        var cols = Physics.OverlapBox (this.transform.position, new Vector3 (0.5f, 0.5f, 5), am.ac.model.transform.rotation, LayerMask.GetMask ("Player"));
+        if (cols.Length > 0) {
             return cols[0].gameObject;
-        }
-        else return null;
+        } else return null;
     }
 
-    private bool CheckCanAttackPlayer()
-    {
-        if (this == null)
-        {
+    private bool CheckCanAttackPlayer () {
+        if (this == null) {
             return false;
         }
-        var cols = Physics.OverlapBox(this.transform.position, new Vector3(0.5f, 0.5f, 1), am.ac.model.transform.rotation, LayerMask.GetMask("Player"));
-        if (cols.Length > 0)
-        {
+        var cols = Physics.OverlapBox (this.transform.position, new Vector3 (0.5f, 0.5f, 1), am.ac.model.transform.rotation, LayerMask.GetMask ("Player"));
+        if (cols.Length > 0) {
             return true;
-        }
-        else return false;
+        } else return false;
     }
 
-    public bool IsTouched(Transform personTrans, Transform targetTrans)
-    {
+    public bool IsTouched (Transform personTrans, Transform targetTrans) {
         var personPos = personTrans.position;
         var targetPos = targetTrans.position;
-        Vector2 pplanePos = new Vector2(personPos.x, personPos.z);
-        Vector2 tplanePos = new Vector2(targetPos.x, targetPos.z);
+        Vector2 pplanePos = new Vector2 (personPos.x, personPos.z);
+        Vector2 tplanePos = new Vector2 (targetPos.x, targetPos.z);
         //        Debug.Log( Vector2.Distance(pplanePos, tplanePos));
-        if (Vector2.Distance(pplanePos, tplanePos) < 0.5f)
-        {
+        if (Vector2.Distance (pplanePos, tplanePos) < 0.5f) {
             return true;
-        }
-        else
-        {
+        } else {
             return false;
         }
     }
     #endregion
 
     #region Action
-    public void Move(Vector3 direction, float speed = 0.7f, bool run = false)
-    {
+    public void Move (Vector3 direction, float speed = 0.7f, bool run = false) {
         var ac = am.ac as EnemyAC;
         ac.playerInput.Dmag = speed;
         ac.playerInput.Dforward = direction;
         ac.playerInput.run = run;
     }
-    public void Attack()
-    {
+    public void Attack () {
         var ac = am.ac as EnemyAC;
         ac.playerInput.rb = true;
     }
 
-    public void UnAttack()
-    {
+    public void UnAttack () {
         var ac = am.ac as EnemyAC;
         ac.playerInput.rb = false;
     }
 
-    public void Roll(Vector3 direction)
-    {
+    public void Roll (Vector3 direction) {
 
     }
 
-    public void Defense()
-    {
+    public void Defense () {
         var ac = am.ac as EnemyAC;
         ac.playerInput.lb = true;
     }
 
-    public void Reset()
-    {
+    public void Reset () {
         var ac = am.ac as EnemyAC;
         ac.playerInput.Dmag = 0;
         ac.playerInput.Dforward = Vector3.zero;
